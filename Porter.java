f6962fecbb9d0bf525ef35d87477f6d97932df91
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-11-18 01:46:53 +0530

    Was explaining how stemmer works

--- Porter.java
+++ Porter.java
@@ -7,6 +7,17 @@ class Porter{
 		PorterStemmer stemmer = new PorterStemmer();
 		String token1 = stemmer.stem(token0);
 		System.out.println(token1);
+		System.out.println(stemmer.stem("diversity"));
+		System.out.println(stemmer.stem("diversit"));
+		System.out.println(stemmer.stem("diversi"));
 		System.out.println(stemmer.stem("divers"));
+		System.out.println(stemmer.stem("diversY"));
+		System.out.println(stemmer.stem("divers1"));
+		System.out.println(stemmer.stem("diver"));
+		System.out.println(stemmer.stem("dive"));
+		System.out.println(stemmer.stem("div"));
+		System.out.println(stemmer.stem("di"));
+		System.out.println(stemmer.stem("d"));
+		System.out.println(stemmer.stem("foolishness"));
 	}
 }
