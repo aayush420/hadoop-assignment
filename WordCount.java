cee3c350697cd8d5fd17169db29a3dd028446d63
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2021-11-18 01:47:25 +0530

    Wrote the normalized scores to tf file

--- WordCount.java
+++ WordCount.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+import java.lang.Math;
 import java.util.StringTokenizer;
+import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,15 +20,13 @@ import opennlp.tools.stemmer.PorterStemmer;
 
 public class WordCount {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-		PorterStemmer stemmer = new PorterStemmer();
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      PorterStemmer stemmer = new PorterStemmer();
       StringTokenizer itr = new StringTokenizer(value.toString());
       while (itr.hasMoreTokens()) {
         word.set(stemmer.stem(itr.nextToken()));
@@ -31,29 +35,49 @@ public class WordCount {
     }
   }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+  public static class TokenizerDoubleMapper extends Mapper<Object, Text, Text, DoubleWritable> {
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+    private final static DoubleWritable one = new DoubleWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      PorterStemmer stemmer = new PorterStemmer();
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(stemmer.stem(itr.nextToken()));
+        context.write(word, one);
       }
-      result.set(sum);
+    }
+  }
+
+  public static class IntSumReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+    private Properties dfProps = new Properties();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+      InputStream iStream = new FileInputStream("./output1df/part-r-00000");
+      dfProps.load(iStream);
+
+    }
+
+    private DoubleWritable result = new DoubleWritable();
+
+    public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+        throws IOException, InterruptedException {
+      double sum = 0;
+      for (DoubleWritable val : values) {
+        sum += (double) val.get();
+      }
+      result.set((double) sum * Math.log(10000 / (sum + 1)));
       context.write(key, result);
     }
   }
 
-  public static class IntSumReducerPerDoc
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+  public static class IntSumReducerPerDoc extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += 1;
@@ -65,7 +89,7 @@ public class WordCount {
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
+    Job job = Job.getInstance(conf, "documnt frequency");
     job.setJarByClass(WordCount.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducerPerDoc.class);
@@ -73,7 +97,23 @@ public class WordCount {
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    FileOutputFormat.setOutputPath(job, new Path(args[1] + "df"));
+    // Print output file path of the job
+    job.waitForCompletion(true);
+
+    Configuration conf1 = new Configuration();
+    Job job1 = Job.getInstance(conf1, "normalized term frequency");
+    job1.setJarByClass(WordCount.class);
+
+    job1.addCacheFile(new Path(args[1] + "df/part-r-00000").toUri());
+    job1.setMapperClass(TokenizerDoubleMapper.class);
+    // job.setCombinerClass(IntSumReducer.class);
+    job1.setReducerClass(IntSumReducer.class);
+    job1.setOutputKeyClass(Text.class);
+    job1.setOutputValueClass(DoubleWritable.class);
+    FileInputFormat.addInputPath(job1, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job1, new Path(args[1] + "tf"));
+
+    System.exit(job1.waitForCompletion(true) ? 0 : 1);
   }
 }
--- wc.sh
+++ wc.sh
@@ -1,5 +1,5 @@
 alias hadoop=~/tools/hadoop/hadoop-3.3.1/bin/hadoop
 hadoop com.sun.tools.javac.Main WordCount.java 
 jar cf wc.jar WordCount*.class 
-rm -rf output1
+rm -rf output1*
 hadoop jar wc.jar WordCount input1/wiki_tutorial output1
